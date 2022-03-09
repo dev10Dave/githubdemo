a5a8b8bbce87d892632543e21ae7ce680fba407e
Dave Kidd <97473204+dev10Dave@users.noreply.github.com>
2022-03-09 16:20:20 -0700

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Why hello!");
+        System.out.println("I was added from github!!! bwa hahahahaha");
     }
 }
